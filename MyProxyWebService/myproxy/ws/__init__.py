49b822b522d43ad318a942078bbb6dc469bbf444
Philip Kershaw <philipkershaw@users.noreply.github.com>
2012-08-28 15:18:48 +0000

    doesn't need to be declared as a namespace package.

--- MyProxyWebService/myproxy/ws/__init__.py
+++ MyProxyWebService/myproxy/ws/__init__.py
@@ -8,4 +8,3 @@ __copyright__ = "(C) 2010 Science and Technology Facilities Council"
 __license__ = "BSD - see LICENSE file in top-level directory"
 __contact__ = "Philip.Kershaw@stfc.ac.uk"
 __revision__ = '$Id$'
-__import__('pkg_resources').declare_namespace(__name__)
