ed5968c1df984fa3593dc314e82617924d49bc17
Philip Kershaw <philipkershaw@users.noreply.github.com>
2013-12-03 22:54:17 +0000

    Fixed re-structured text formatting.

--- MyProxyClient/setup.py
+++ MyProxyClient/setup.py
@@ -30,25 +30,33 @@ setup(
     version =         	'1.3.1',
     description =     	'MyProxy Client',
     long_description = 	'''\
-Python implementation of the client interface to the MyProxy credential 
-management service (http://grid.ncsa.illinois.edu/myproxy/).
+MyProxy Client Package
+======================
+This a pure* Python implementation of a client to the MyProxy Credential
+Management Server (http://grid.ncsa.uiuc.edu/myproxy/)
 
-The code has been extended from an original program myproxy_logon by Tom Uram of
-ANL.
+* i.e. MyProxy C client libraries are not required for this package. 
 
-1.3.1
+It uses pyOpenSSL to make an SSL connection to the server following the
+messaging interface as outlined in: http://grid.ncsa.uiuc.edu/myproxy/protocol/
+
+The code is based on an original program myproxy_logon by Tom Uram of ANL.
+
+Tests
 =====
- * fix to MyProxyClient.readProxyFile and MyProxyClient.writeProxyFile - make 
-   sure it picks up filePath value if set.   Thanks to Nicolas Carenton, IPSL 
-   for spotting.
-   
-1.3.0
-=====
- * Added capability for SSL-client cert based authentication for logon without 
-pass-phrase.  This enables authorised retrievers to get a new delegation for
-credentials belonging to another user.  
- * Also add support for credential name along with user name.
- * Fix to script credential lifetime setting for script invocation.
+Unit test module with test files is in test/.  See the README in that directory.
+This package has been tested on Linux Ubuntu Gutsy Gibbon, and Windows XP
+Professional SP2
+
+Documentation
+=============
+Epydoc generated documentation is available in documentation/.  run the 
+Makefile to regenerate if required.
+
+Thanks
+======
+ * to OMII-UK for funding development of NDG Security (2007-2008)
+ * Tom Uram who wrote the myproxy_logon program on which this package is based.
 ''',
     author =          	'Philip Kershaw',
     author_email =    	'Philip.Kershaw@stfc.ac.uk',
